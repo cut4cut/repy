2204d81c7bba29958bcbed7dcae71c006cd8f9e1
a1 <a1@machine.local>
2019-07-16 12:29:55 +0300

    Fix style

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from repy.loader import File_loader
 from repy.xml import generate_ET_from_JSON, convert_JSON2metaJSON, Document_model
 
 		####################################################
-		#  ВЫГРУЗКА ДАННЫХ 						           #
+		#  ВЫГРУЗКА ДАННЫХ 								   #
 		#						   						   #
 		#  Загружаем исходные данные, описание пространст- #
 		#  ва имен и описание стрктуры заголовочного тега. #
@@ -34,7 +34,7 @@ ns_dic = fl.read_json(ns_file_path)
 
 		# Словарь для добавления метатегов из исходного JSON
 temp_data_dic = {
-    '#': []
+	'#': []
 }
 		
 		####################################################
@@ -44,25 +44,25 @@ temp_data_dic = {
 		# в словаре информацию о значениях аттрибутов по   #
 		# умолчанию и выделять значением равным '?' аттри- #
 		# буты для которых нужно искать значения в файле   #
-		# JSON c исходными данными.                        #
+		# JSON c исходными данными.						#
 		####################################################
 
 series_atrib_dic = {'FREQ': 'A', 'TAX_KIND': '?', 'TIME_PERIOD': '2018'}
 series_atrib_maper = {'TAX_KIND': 'id'}
 
 		####################################################
-		# Работа с тегом Obs    						   #
+		# Работа с тегом Obs							   #
 		# 												   #
 		# Тк при конвертации из JSON в XML происходит уве- # 
 		# лечение размерности данных - нужно явно указать  #
-		# по каким полям будет генерация тегов Obs.        #
-		#                                                  #
+		# по каким полям будет генерация тегов Obs.		   #
+		#												   #
 		# obs_list - список полей записи JSON для создания #
-		#            серии тегов Obs                       #
-		#                            					   #
+		#			серии тегов Obs					       #
+		#												   #
 		# obs_atrib_dic - словарь аттрибутов каждого тега  #
-		#                 Obs. Виесто знака '§' подставля- #
-		#                 ется значение из obs_list        #
+		#				 Obs. Виесто знака '§' подставля-  #
+		#				 ется значение из obs_list		   #
 		####################################################
 
 obs_list = ['accrued', 'consolidated', 'enrolled', 'federal']
@@ -70,17 +70,17 @@ obs_atrib_dic = {'OBS_VALUE': '?', 'UNIT_MEASURE': 'THS', 'type': '§'}
 
 		# Сдлварь для записи значение obs_list в стиле XML
 obs_atrib_maper = {  'accrued': 'ACCRUED', 
-                     'consolidated': 'CONSOLIDATED', 
-                     'enrolled': 'ENROLLED', 
-                     'federal': 'FEDERAL'}
+					 'consolidated': 'CONSOLIDATED', 
+					 'enrolled': 'ENROLLED', 
+					 'federal': 'FEDERAL'}
 
 prep_data_dic = convert_JSON2metaJSON(  raw_data_dic, 
 										temp_data_dic, 
-			          					series_atrib_dic, 
-			          					series_atrib_maper,
-			          					obs_list,
-			          					obs_atrib_dic,
-			          					obs_atrib_maper )
+					  					series_atrib_dic, 
+					  					series_atrib_maper,
+					  					obs_list,
+					  					obs_atrib_dic,
+					  					obs_atrib_maper )
 
 fl.write_json('./data/output/prep_data_01.json', prep_data_dic)
 
@@ -88,7 +88,7 @@ fl.write_json('./data/output/prep_data_01.json', prep_data_dic)
 		#  СОЗДАНИЕ МОДЕЛИ ДОКУМЕНТА XML				   #
 		#						   						   #
 		#  Создание корневого тега и основных сегментов бу-#
-		#  дущего XML документа.    				       #
+		#  дущего XML документа.						   #
 		####################################################
 
 		# Создание qname корневого тега 
@@ -105,11 +105,11 @@ dm.create_segment('DataSet', 'message', 'Structure', 'message')
 root = dm.get_root()
 
 		####################################################
-		#  ЗАПОЛНЕНИЕ СЕГМЕНТОВ ДОКУМЕНТА XML		       #
+		#  ЗАПОЛНЕНИЕ СЕГМЕНТОВ ДОКУМЕНТА XML			   #
 		#						   						   #
 		#  Происходит поиск по имени нужного сегмента, по  #
 		#  metaJSON просходит создание ET.elemnt и рекур-  #
-		#  сивное добавление к корню.            		   #
+		#  сивное добавление к корню.					   #
 		####################################################
 
 generate_ET_from_JSON(root, header_dic, 'Header', ns_dic)
